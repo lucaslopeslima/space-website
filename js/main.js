e3f4914e197af26017756eaca2d0ae6441f4610f
Lucas L. Lima <lucaslopes.developer@gmail.com>
2022-11-26 22:25:04 -0300

    tecnology menu done

--- js/main.js
+++ js/main.js
@@ -120,24 +120,42 @@ dMenuTrasitions()
 
 
 
-/*          console.log(this.id)
-            console.log(this.className)
-            if (this.id == moon || this.className){
-                console.log(this.id)
-                console.log('passou')
-                destinationContent.innerHTML = '<div class="destination-img"><img src="assets/destination/image-moon.webp" alt=""></div><div class="destination-container"><nav class="destination-menu"><div class="d-menu destination-btn" id="moon">Moon</div><div class="d-menu " id="mars">Mars</div><div class="d-menu " id="europa">Europa</div><div class="d-menu " id="titan">Titan</div></nav><div class="destination-txt"><h2 class="destination-title">Moon</h2><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam iure commodi quibusdam ratione, debitis illo, ea dicta obcaecati iste provident est architecto quam quidem suscipit excepturi dolorum ullam laborum voluptatem?</p><div class="destination-line"></div><div class="destination-info"><div class="destination-distance"><span>AVG. distance</span><h2>384.400 Km</h2></div><div class="destination-travel-time"><span>est. travel time</span><h2>3 days</h2></div></div></div><!--destination-txt--></div><!-- /.destination-container -->'
+
+const tMenu = document.querySelectorAll('.t-menu')
+let tecnologyContent = document.querySelector('.tecnology-content')
+let tContent = document.getElementById('tContent')
+console.log(tMenu + ' t-menu')
+console.log(tecnologyContent)
+console.log(tContent)
+
+
+function tMenuTrasitions(){
+    for(let i = 0; i < tMenu.length; i++){
+        tMenu[i].addEventListener('click', function(){
+            let currenttMenu = document.querySelectorAll('.t-menu-active')
+            currenttMenu[0].className = currenttMenu[0].className.replace('t-menu-active', '')
+            this.className += ' t-menu-active'
+            
+        })
+    }
+    
+    tContent.addEventListener('click', (e) =>{
+        const id = e.target.id
+        //console.log(id + ' esse é o id')
+            if (id == '1'){
+                console.log('passou o 1')
+                tContent.innerHTML = '<div class="tecnology-menu"><div class="t-menu t-menu-active" id="1"><h2>1</h2></div><div class="t-menu " id="2"><h2>2</h2></div><div class="t-menu " id="3"><h2>3</h2></div></div><div class="tec-content-wrap"><div class="tec-txt"><span>the terminology...</span><h2>launch vehicle</h2><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta itaque perspiciatis possimus veniam molestias dolore quas voluptatibus nemo quae. Recusandae sed nihil praesentium repellendus ipsa numquam fuga deleniti perferendis debitis?</p></div><div class="tec-img"><img src="assets/technology/image-launch-vehicle-portrait.jpg" alt=""></div></div>'
+                
             }
-            if(this.id == mars){
-                console.log('passou')
-                destinationContent.innerHTML = '<div>ola</div>'
-                destinationContent.innerHTML = '<div class="destination-img"><img src="assets/destination/image-moon.webp" alt=""></div><div class="destination-container"><nav class="destination-menu"><div class="d-menu " id="moon">Moon</div><div class="d-menu destination-btn" id="mars">Mars</div><div class="d-menu " id="europa">Europa</div><div class="d-menu " id="titan">Titan</div></nav><div class="destination-txt"><h2 class="destination-title">Mars</h2><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam iure commodi quibusdam ratione, debitis illo, ea dicta obcaecati iste provident est architecto quam quidem suscipit excepturi dolorum ullam laborum voluptatem?</p><div class="destination-line"></div><div class="destination-info"><div class="destination-distance"><span>AVG. distance</span><h2>384.400 Km</h2></div><div class="destination-travel-time"><span>est. travel time</span><h2>3 days</h2></div></div></div><!--destination-txt--></div><!-- /.destination-container -->'
+            if(id == '2'){
+                console.log('passou o 2')
+                tContent.innerHTML = '<div class="tecnology-menu"><div class="t-menu " id="1"><h2>1</h2></div><div class="t-menu t-menu-active" id="2"><h2>2</h2></div><div class="t-menu " id="3"><h2>3</h2></div></div><div class="tec-content-wrap"><div class="tec-txt"><span>the terminology...</span><h2>spaceport</h2><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta itaque perspiciatis possimus veniam molestias dolore quas voluptatibus nemo quae. Recusandae sed nihil praesentium repellendus ipsa numquam fuga deleniti perferendis debitis?</p></div><div class="tec-img"><img src="assets/technology/image-spaceport-portrait.jpg" alt=""></div></div>'
+                
             }
-            if(this.id == europa){
-                console.log(this.id)
-               destinationContent.innerHTML = '<div class="destination-img"><img src="assets/destination/image-moon.webp" alt=""></div><div class="destination-container"><nav class="destination-menu"><div class="d-menu " id="moon">Moon</div><div class="d-menu " id="mars">Mars</div><div class="d-menu destination-btn" id="europa">Europa</div><div class="d-menu " id="titan">Titan</div></nav><div class="destination-txt"><h2 class="destination-title">Europa</h2><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam iure commodi quibusdam ratione, debitis illo, ea dicta obcaecati iste provident est architecto quam quidem suscipit excepturi dolorum ullam laborum voluptatem?</p><div class="destination-line"></div><div class="destination-info"><div class="destination-distance"><span>AVG. distance</span><h2>384.400 Km</h2></div><div class="destination-travel-time"><span>est. travel time</span><h2>3 days</h2></div></div></div><!--destination-txt--></div><!-- /.destination-container -->'
+            if(id == '3'){
+                console.log('passou o 3')
+                tContent.innerHTML = '<div class="tecnology-menu"><div class="t-menu " id="1"><h2>1</h2></div><div class="t-menu " id="2"><h2>2</h2></div><div class="t-menu t-menu-active" id="3"><h2>3</h2></div></div><div class="tec-content-wrap"><div class="tec-txt"><span>the terminology...</span><h2>space capsule</h2><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta itaque perspiciatis possimus veniam molestias dolore quas voluptatibus nemo quae. Recusandae sed nihil praesentium repellendus ipsa numquam fuga deleniti perferendis debitis?</p></div><div class="tec-img"><img src="assets/technology/image-space-capsule-portrait.jpg" alt=""></div></div>'
             }
-            if(this.id == titan){
-                console.log(this.id)
-                destinationContent.innerHTML = '<div class="destination-img"><img src="assets/destination/image-moon.webp" alt=""></div><div class="destination-container"><nav class="destination-menu"><div class="d-menu " id="moon">Moon</div><div class="d-menu " id="mars">Mars</div><div class="d-menu " id="europa">Europa</div><div class="d-menu destination-btn" id="titan">Titan</div></nav><div class="destination-txt"><h2 class="destination-title">Titan</h2><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam iure commodi quibusdam ratione, debitis illo, ea dicta obcaecati iste provident est architecto quam quidem suscipit excepturi dolorum ullam laborum voluptatem?</p><div class="destination-line"></div><div class="destination-info"><div class="destination-distance"><span>AVG. distance</span><h2>384.400 Km</h2></div><div class="destination-travel-time"><span>est. travel time</span><h2>3 days</h2></div></div></div><!--destination-txt--></div><!-- /.destination-container -->'
-            } 
-            */
+    })
+}
+tMenuTrasitions() 
